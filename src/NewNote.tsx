4a70015c998ea22b976cb7cf4996e60942ada73e
arya perdana <arya.perdana@happy5.co>
2022-11-27 19:41:30 +0700

    create NewNote

---
+++ src/NewNote.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NoteData, Tag } from './App';
+import NoteForm from './NoteForm';
+
+type NewNotePops = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export default function NewNote({
+  onSubmit,
+  onAddTag,
+  availableTags
+}: NewNotePops) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+}
